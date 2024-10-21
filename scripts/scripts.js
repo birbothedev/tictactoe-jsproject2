08ab0b94f09224788960fc6e41dcde9eab55af5a
ginafender <123966430+ginafender@users.noreply.github.com>
2024-10-21 13:05:26 -0400

    added starter files

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+const title = document.getElementById('title');
+title.style.display = 'none';
+const gameContainer = document.getElementById('gameContainer');
+gameContainer.style.display = 'none';
+
+document.getElementById('playButton').addEventListener('click', function() {
+    this.style.display = 'none'; 
+    title.style.display = 'block';
+    gameContainer.style.display = 'grid';
+    hoverImages();
+});
