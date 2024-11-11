eb35f023d40f04cfcfa18f806ef287fe676be22a
ginafender <123966430+ginafender@users.noreply.github.com>
2024-11-11 09:25:57 -0500

    game container appearing as it should
    the issue was with the javascript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,10 @@
 const title = document.getElementById('title');
-title.style.display = 'none';
+title.style.visibility = 'hidden'; // or opacity: 0;
 const gameContainer = document.getElementById('gameContainer');
-gameContainer.style.display = 'none';
+gameContainer.style.visibility = 'hidden'; // or opacity: 0;
 
 document.getElementById('playButton').addEventListener('click', function() {
     this.style.display = 'none'; 
-    title.style.display = 'block';
-    gameContainer.style.display = 'block';
+    title.style.visibility = 'visible'; // or opacity: 1;
+    gameContainer.style.visibility = 'visible'; // or opacity: 1;
 });
